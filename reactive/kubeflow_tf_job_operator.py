d2ba59fa21fc29b367b333da5e583e3b65210be6
Cory Johns <johnsca@gmail.com>
2018-07-09 18:08:24 -0400

    Add entry point command

--- reactive/kubeflow_tf_job_operator.py
+++ reactive/kubeflow_tf_job_operator.py
@@ -11,19 +11,27 @@ from charms.layer.basic import pod_spec_set
 def start_charm():
     layer.status.maintenance('configuring tf-job-operator container')
 
+    conf_dir = '/etc/tf_operator'
+    conf_file = 'controller_config_file.yaml'
     pod_spec_set(yaml.dump({
         'containers': [
             {
                 'name': 'tf-job-operator',
                 'image': ('gcr.io/kubeflow-images-public/'
                           'tf_operator:v20180329-a7511ff'),
-                'command': [],
+                'command': [
+                    '/opt/mlkube/tf-operator',
+                    '--controller-config-file={}/{}'.format(conf_dir,
+                                                            conf_file),
+                    '--alsologtostderr',
+                    '-v=1',
+                ],
                 'files': [
                     {
                         'name': 'configs',
-                        'mountPath': '/etc/tf_operator',
+                        'mountPath': conf_dir,
                         'files': {
-                            'controller_config_file.yaml': yaml.dump({
+                            conf_file: yaml.dump({
                                 'grpcServerFilePath': (
                                     '/opt/mlkube/grpc_tensorflow_server/'
                                     'grpc_tensorflow_server.py'
