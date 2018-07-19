c47f1d25b1fdb82a31617dd6eb0200c490c5aa75
Cory Johns <johnsca@gmail.com>
2018-07-19 16:21:51 -0400

    Use resource for image

--- reactive/kubeflow_tf_job_operator.py
+++ reactive/kubeflow_tf_job_operator.py
@@ -1,24 +1,33 @@
 import yaml
 
 from charms.reactive import set_flag
-from charms.reactive import when_not
+from charms.reactive import when, when_not
 
 from charms import layer
-from charms.layer.basic import pod_spec_set
 
 
+@when_not('layer.docker-resource.tf-operator-image.fetched')
+def fetch_image():
+    layer.docker_resource.fetch('tf-operator-image')
+
+
+@when('layer.docker-resource.tf-operator-image.fetched')
 @when_not('charm.kubeflow-tf-job-operator.started')
 def start_charm():
     layer.status.maintenance('configuring container')
 
     conf_dir = '/etc/tf_operator'
     conf_file = 'controller_config_file.yaml'
-    pod_spec_set(yaml.dump({
+    image_info = layer.docker_resource.get_info('tf-operator-image')
+    layer.caas_base.pod_spec_set({
         'containers': [
             {
                 'name': 'tf-job-operator',
-                'image': ('gcr.io/kubeflow-images-public/'
-                          'tf_operator:v20180329-a7511ff'),
+                'imageDetails': {
+                    'imagePath': image_info.registry_path,
+                    'username': image_info.username,
+                    'password': image_info.password,
+                },
                 'command': [
                     '/opt/mlkube/tf-operator',
                     '--controller-config-file={}/{}'.format(conf_dir,
@@ -48,7 +57,7 @@ def start_charm():
                 ],
             },
         ],
-    }))
+    })
 
     layer.status.maintenance('creating container')
     set_flag('charm.kubeflow-tf-job-operator.started')
